2d8e51c21bb75f33ca59c6a49a4357a29eebd625
Yash Srivastav <yash111998@gmail.com>
2023-12-14 01:31:04 +0000

    Use quadratic equation formula for 6th question

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -12,24 +12,45 @@ pub fn part_one(input: &str) -> Option<u32> {
     let (_, races) = parse_input(input).ok()?;
     let mut product = 1;
     for race in races {
-        let mut win_count = 0;
-        for time in 0..race.time {
-            let remaining_time = race.time - time;
-            let speed = time;
-            let distance_covered = speed * remaining_time;
-            if distance_covered > race.distance {
-                win_count += 1;
-            }
+        let discriminant = ((race.time * race.time - 4 * race.distance) as f32).sqrt();
+        let min_time = (race.time as f32 - discriminant) / 2_f32;
+        let max_time = (race.time as f32 + discriminant) / 2_f32;
+        let min_time_rounded = min_time.ceil() as u32;
+        let max_time_rounded = max_time.floor() as u32;
+
+        let min_included = min_time == min_time.ceil();
+        let max_included = max_time == max_time.floor();
+        let mut win_count = max_time_rounded - min_time_rounded + 1;
+        if min_included {
+            win_count -= 1;
         }
-        if win_count > 0 {
-            product *= win_count;
+        if max_included {
+            win_count -= 1;
         }
+        product *= win_count;
     }
     Some(product)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let (_, races) = parse_input(input).ok()?;
+
+    let mut actual_time = 0_f64;
+    let mut actual_distance = 0_f64;
+    for race in races {
+        let multiply_by_time = (race.time as f64).log10().ceil();
+        let multiply_by_distance = (race.distance as f64).log10().ceil();
+        actual_time = actual_time * 10_f64.powf(multiply_by_time) + race.time as f64;
+        actual_distance =
+            actual_distance * 10_f64.powf(multiply_by_distance) + race.distance as f64;
+    }
+    let discriminant = (actual_time * actual_time - 4_f64 * actual_distance).sqrt();
+    let min_time = (actual_time - discriminant) / 2_f64;
+    let max_time = (actual_time + discriminant) / 2_f64;
+    let min_time_rounded = min_time.ceil() as u32;
+    let max_time_rounded = max_time.floor() as u32;
+    let win_count = max_time_rounded - min_time_rounded + 1;
+    Some(win_count)
 }
 
 #[derive(Debug, PartialEq)]
@@ -84,6 +105,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(71503));
     }
 }
