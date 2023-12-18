7b6f7d6b97b1a75df2242567cd813dc092599254
Yash Srivastav <yashsriv@meta.com>
2023-12-18 17:09:08 +0000

    Solve Q10 part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -101,8 +101,87 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some((loop_length + 1) / 2)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+fn get_tile_type_without_start(grid: &[Vec<TileType>], (row, column): (usize, usize)) -> &TileType {
+    if !matches!(grid[row][column], TileType::Start) {
+        &grid[row][column]
+    } else {
+        let has_north_connection = row > 0
+            && matches!(
+                grid[row - 1][column],
+                TileType::NorthSouth | TileType::SouthEast | TileType::SouthWest
+            );
+        let has_south_connection = row < grid.len() - 1
+            && matches!(
+                grid[row + 1][column],
+                TileType::NorthSouth | TileType::NorthEast | TileType::NorthWest
+            );
+        let has_west_connection = column > 0
+            && matches!(
+                grid[row][column - 1],
+                TileType::EastWest | TileType::NorthEast | TileType::SouthEast
+            );
+        let has_east_connection = column < grid[row].len() - 1
+            && matches!(
+                grid[row][column + 1],
+                TileType::EastWest | TileType::NorthWest | TileType::SouthWest
+            );
+
+        match (
+            has_north_connection,
+            has_south_connection,
+            has_east_connection,
+            has_west_connection,
+        ) {
+            (true, true, false, false) => &TileType::NorthSouth,
+            (false, false, true, true) => &TileType::EastWest,
+            (true, false, true, false) => &TileType::NorthEast,
+            (true, false, false, true) => &TileType::NorthWest,
+            (false, true, true, false) => &TileType::SouthEast,
+            (false, true, false, true) => &TileType::SouthWest,
+            _ => panic!("Unexpected scenario"),
+        }
+    }
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let (_, grid) = parse_input(input).ok()?;
+    let (start_i, start_j) = find_start(&grid)?;
+
+    let mut visited = HashSet::new();
+    visited.insert((start_i, start_j));
+
+    let mut result = get_next((start_i, start_j), &grid, &visited);
+    while let Some((i, j)) = result {
+        visited.insert((i, j));
+        result = get_next((i, j), &grid, &visited);
+    }
+
+    let mut area = 0;
+    for (i, row) in grid.iter().enumerate() {
+        for (j, _) in row.iter().enumerate() {
+            let is_visited = visited.contains(&(i, j));
+            if is_visited {
+                continue;
+            }
+
+            let mut is_inside = false;
+            let mut location = (i, j);
+            while location.0 < grid.len() && location.1 < grid[location.0].len() {
+                let tile_type = get_tile_type_without_start(&grid, location);
+                if visited.contains(&location)
+                    && !matches!(tile_type, TileType::NorthEast | TileType::SouthWest)
+                {
+                    is_inside = !is_inside;
+                }
+                location = (location.0 + 1, location.1 + 1);
+            }
+
+            if is_inside {
+                area += 1;
+            }
+        }
+    }
+    Some(area)
 }
 
 #[derive(Debug)]
@@ -161,12 +240,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(8));
+        assert_eq!(result, Some(23));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
